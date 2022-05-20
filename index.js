1afd42393b44d9c2c0e4547bacfb4b2a96bf4429
Ashton Tobar <+>
2022-05-20 14:33:47 -0500

    commit tree 285fb82418ff96de7d4d9f5072b018b058ac5b96

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+  name: "Ash",
+  streetAddress: "123 Main",
+};
+
+//Function One - COMPLETE
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value,
+  };
+}
+updateEmployeeWithKeyAndValue(employee, "Sam", "11 Broadway");
+
+//function two
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+  employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+
+  return employee;
+}
+
+//function three
+function deleteFromEmployeeByKey(obj, key, value) {
+  let newEmployee = {
+    ...obj,
+    [key]: value,
+  };
+  delete newEmployee.name;
+  return newEmployee;
+}
+
+
+//function four - COMPLETE
+function destructivelyDeleteFromEmployeeByKey() {
+  delete employee.name;
+  return employee;
+}
